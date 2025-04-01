f9c78cc884660e135f12a440537574f686118c99
fvergaracl <fvergaracl@gmail.com>
2025-04-01 17:40:28 +0200

    fix (sentry):config in server and frontend

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,44 +1,9 @@
-// sentry.client.config.ts
+import { getSentryDSN } from "./src/config/api"
+import * as Sentry from "@sentry/nextjs"
+import { browserTracingIntegration } from "@sentry/browser"
 
-/**
- * Configures the initialization of Sentry on the client-side.
- * This configuration is applied whenever a user loads a page in their browser.
- * Documentation: https://docs.sentry.io/platforms/javascript/guides/nextjs/
- */
-
-import * as Sentry from "@sentry/nextjs";
-import { getSentryEnabled } from "@/config/api";
-
-// Ensure the Sentry DSN is provided before initializing
-const SENTRY_DSN = getSentryEnabled();
-const SENTRY_ENABLED = process?.env?.NEXT_PUBLIC_SENTRY_ENABLED || false;
-const ENVIRONMENT = process?.env?.NEXT_PUBLIC_SENTRY_ENVIRONMENT;
-
-if (SENTRY_DSN && SENTRY_ENABLED) {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    environment: ENVIRONMENT || "development",
-
-    // Adjust the tracesSampleRate in production for performance monitoring.
-    // Use `tracesSampler` for more granular control if needed.
-    tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1.0,
-
-    // Enable debugging in non-production environments for better troubleshooting.
-    debug: process.env.NODE_ENV !== "production",
-
-    // Configure session replay sampling rates.
-    // Adjust these values based on your requirements in production.
-    replaysOnErrorSampleRate: 1.0, // Record all sessions with errors
-    replaysSessionSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1.0, // Sample a fraction of sessions
-
-    // Sentry integrations: Include the Replay feature for session recordings.
-    integrations: [
-      Sentry.replayIntegration({
-        maskAllText: true, // Mask all text inputs to avoid sensitive data leaks
-        blockAllMedia: true, // Block all media elements from being captured
-      }),
-    ],
-  });
-} else {
-  console.warn("Sentry DSN is not provided. Sentry will not be initialized.");
-}
+Sentry.init({
+  dsn: getSentryDSN(),
+  integrations: [browserTracingIntegration()],
+  tracesSampleRate: 1.0
+})
--- src/sentry.server.config.ts
+++ src/sentry.server.config.ts
@@ -4,3 +4,4 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1.0,
 })
+
