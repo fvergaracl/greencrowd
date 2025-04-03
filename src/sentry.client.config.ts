b94563e8c3db9dda9cddb764c506fc5b9dcede8c
fvergaracl <fvergaracl@gmail.com>
2025-04-03 16:38:53 +0200

    feat(sentry): simplify Sentry DSN configuration by using a constant variable

--- src/sentry.client.config.ts
+++ src/sentry.client.config.ts
@@ -2,11 +2,15 @@ import { getSentryDSN } from "./config/api"
 import * as Sentry from "@sentry/nextjs"
 import { browserTracingIntegration } from "@sentry/browser"
 
+const sentryDSN =
+  getSentryDSN() ||
+  process?.env?.NEXT_PUBLIC_SENTRY_DSN_FRONTEND ||
+  (typeof window !== "undefined"
+    ? window?.__ENV__?.NEXT_PUBLIC_SENTRY_DSN_FRONTEND
+    : undefined)
+
 Sentry.init({
-  dsn:
-    getSentryDSN() ||
-    process?.env?.NEXT_PUBLIC_SENTRY_DSN_FRONTEND ||
-    window?.__ENV__?.NEXT_PUBLIC_SENTRY_DSN_FRONTEND,
+  dsn: sentryDSN,
   integrations: [browserTracingIntegration()],
   tracesSampleRate: 1.0
 })
