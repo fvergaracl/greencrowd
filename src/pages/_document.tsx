2c6c349dfb40a39bf3463a2a2cd27a86f5e2d0f8
fvergaracl <fvergaracl@gmail.com>
2025-04-04 12:55:24 +0200

    feat(document): enhance mobile support with additional meta tags and icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,16 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        {/* 👇 Aquí va tu meta viewport */}
         <meta
           name='viewport'
           content='width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover'
         />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/icons/icon-192x192.png' />
+        <meta name='theme-color' content='#000000' />
       </Head>
       <body>
         <Main />
